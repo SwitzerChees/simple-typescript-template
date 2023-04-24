dc804449f94625a73e1339141afab94ff7cd6786
switzerchees <patrickmichel96@yahoo.de>
2023-04-24 19:32:28 +0200

    Add live reload for client

--- server/index.ts
+++ server/index.ts
@@ -1,17 +1,42 @@
 import express, { Request, Response } from 'express'
-import ViteExpress from 'vite-express'
+import connectLiveReload from 'connect-livereload'
+import livereload from 'livereload'
 import { loadAPI } from './api'
+import http from 'http'
+import { resolve, dirname } from 'path'
+
+const __dirname = resolve(dirname(''))
 
 const app = express()
 
 loadAPI(app)
 
-app.get('/', (req: Request, res: Response) => {
-  res.sendFile('index.html', { root: 'client' })
+// create a livereload server
+const env = process.env.NODE_ENV || 'development'
+if (env !== 'production') {
+  const liveReloadServer = livereload.createServer()
+  liveReloadServer.watch(__dirname + '/client')
+  liveReloadServer.server.once('connection', () => {
+    setTimeout(() => {
+      liveReloadServer.refresh('/')
+    }, 100)
+  })
+  // use livereload middleware
+  app.use(connectLiveReload())
+}
+
+// deliver static files from the client folder like css, js, images
+app.use(express.static('client'))
+
+// route for the homepage
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/client/index.html')
 })
 
 export const viteNodeApp = app
 
-if (process.env.NODE_ENV === 'production') {
-  ViteExpress.listen(app, 3000, () => console.log('Server is listening...'))
+if (env === 'production') {
+  http.createServer(app).listen(3000, () => {
+    console.log('Server is listening!')
+  })
 }
