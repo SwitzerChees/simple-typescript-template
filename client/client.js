376822ac45c57a361b7b0abcdde6650405b45226
switzerchees <patrickmichel96@yahoo.de>
2023-04-24 20:21:34 +0200

    Integrate mariadb, docker-compose and split server and client dev script

--- client/client.js
+++ client/client.js
@@ -1,1 +1,11 @@
-console.log('Tets2222')
+if (location.host.includes('localhost')) {
+  // Load livereload script if we are on localhost
+  document.write(
+    '<script src="http://' +
+      (location.host || 'localhost').split(':')[0] +
+      ':35729/livereload.js?snipver=1"></' +
+      'script>'
+  )
+}
+
+console.log('This is a Test')
--- server/api/index.ts
+++ server/api/index.ts
@@ -1,13 +1,1 @@
-import { Request, Response, Express } from 'express'
-
-export class API {
-  app: Express
-  constructor(app: Express) {
-    this.app = app
-    this.app.get('/hello', this.sayHello)
-  }
-
-  public sayHello(req: Request, res: Response) {
-    res.send('Hello There!')
-  }
-}
+export * from './api'
